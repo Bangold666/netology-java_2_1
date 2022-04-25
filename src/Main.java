466ce4965cb54ee6cd8230f39ed534a89902fbff
Shabalin Sergey <sergeyshm38@gmail.com>
2022-04-25 18:15:10 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,8 @@ public class Main {
         int milesperPrice = 1; // Мили за каждые 20 рублей
         int miles = 0;// Конечная ячейка для бонусной или
 
-
         milesperPrice = ticketPrice / 20;
         miles = miles + milesperPrice;
-
 
         System.out.println((miles) + " Миль ");//Вывод окончательных Миль
     }
